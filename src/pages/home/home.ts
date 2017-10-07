ed13a8566b7e786d3431e160f9389655363daa16
wf-chris-li <nwe.cli@westfield.com>
2017-10-07 05:34:12 -0700

    chat local working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from "@angular/http";
+import { NgZone } from "@angular/core";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  messages: string[] = ['something'];
+  chatBox = "";
   constructor(public navCtrl: NavController) {
-
+    this.messages = [];
+    this.messages.push("msg 1")
+    this.messages.push("msg 2")
+    this.chatBox = "type your message here...";
   }
-
+  send(message) {
+    if(message && message != "") {
+        this.messages.push(message);
+        //this.socket.emit("chat_message", message);
+    }
+    this.chatBox = "";
+}
 }
